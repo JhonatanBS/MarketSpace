05ebbea07ece38244e1f74988dbc15dcbf4cc51a
JhonatanBS <jhonatanb.s4nt0s@gmail.com>
2024-01-22 11:04:04 -0300

    Creating Provider

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,7 +9,7 @@ import { THEME } from './src/theme';
 import { Loading } from "@components/Loading";
 
 import { Routes } from '@routes/index';
-import { AuthContext } from '@contexts/AuthContext';
+import { AuthContextProvider } from '@contexts/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold });
@@ -21,18 +21,9 @@ export default function App() {
         barStyle={"dark-content"}
         backgroundColor={"transparent"}
         />
-        <AuthContext.Provider value={{
-          user: {
-            id: "1",
-            avatar: "jhonatan.png",
-            name: "Jhonatan",
-            email: "jhonatan@gmail.com",
-            password: "1234567",
-            phone: "6146565656"
-          }
-        }}>
+        <AuthContextProvider>
           { fontsLoaded ? <Routes /> : <Loading />}
-        </AuthContext.Provider>
+        </AuthContextProvider>
         
     </NativeBaseProvider>
   );
--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { ReactNode, createContext } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
 
@@ -6,4 +6,25 @@ export type AuthContextDataProps = {
   user: UserDTO
 }
 
+type AuthContextProviderProps = {
+  children: ReactNode;
+}
+
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  return(
+    <AuthContext.Provider value={{
+      user: {
+        id: "1",
+        avatar: "jhonatan.png",
+        name: "Jhonatan",
+        email: "jhonatan@gmail.com",
+        password: "1234567",
+        phone: "6146565656"
+      }
+    }}>
+      { children }
+    </AuthContext.Provider>
+  )
+}
